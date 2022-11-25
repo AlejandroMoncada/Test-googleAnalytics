e153d1029046a18c210d4a37ef1fb3543244bd3a
AlejandroMoncada <alejandrolopezmoncada10@gmail.com>
2022-11-24 22:08:12 -0500

    Test one of google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import './App.css'
 import ReactGA from 'react-ga'
 
 function App() {
-  const TRACKING_ID = 'UA-250149670-1'
+  const TRACKING_ID = 'UA-250149670-2'
   ReactGA.initialize(TRACKING_ID)
   return (
     <>
+    <h1>Hola mundo</h1>
     <ContactUs/>
     </>
   )
